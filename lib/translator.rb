ea3a89d93fec09a7d81e2c1a329de08b4d7addf2
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2021-01-06 01:30:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(file, w_emoticon)
   emoticons = load_library(file)
   sorry_message = "Sorry, that emoticon was not found"
   #binding.pry
-  j_emoticon = emoticons.each do |k, v|
+  j_emoticon = emoticons.find do |k, v|
     w_emoticon = emoticons[k][:english]
     #binding.pry
     if emoticons[k].value?(w_emoticon)
@@ -40,7 +40,7 @@ def get_english_meaning(file, j_emoticon)
   emoticons = load_library(file)
   sorry_message = "Sorry, that emoticon was not found"
   #binding.pry
-  english = emoticons.each do |k,v|
+  english = emoticons.find do |k,v|
     j_emoticon = emoticons[k][:japanese]
     #binding.pry
     if emoticons[k].value?(j_emoticon)
